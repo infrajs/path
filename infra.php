865f539537ce51995360e24c2cb07a4571f9546a
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-23 23:01:33 +0400

    data/update file remove cache folder

--- infra.php
+++ infra.php
@@ -9,12 +9,14 @@ if (!is_file('vendor/autoload.php')) {
 require_once('vendor/autoload.php');
 
 $path = Path::$conf;
+
 if (!is_dir($path['cache'])) {
 	Config::$install = true;
 	mkdir($path['cache']);
 }
-if (is_file($path['data'].'update')) {
+if ($path['fs']&&is_file($path['data'].'update')) {
 	header('Infrajs-Path-Update:true');
+	Path::fullrmdir($path['cache']);
 	Config::$install = true;
 	unlink($path['data'].'update');
 }
