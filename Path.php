0d098b7d8fb2b47153657733f14a402a03ff59bf
Anton <a.kiyatkin@itlf.ru>
2016-12-21 20:51:53 +0400

    fix notice

--- Path.php
+++ Path.php
@@ -316,7 +316,7 @@ class Path {
 		$str = preg_replace('/^\s+/', '', $str);
 		$str = preg_replace('/\s+$/', '', $str);
 		$str = preg_replace('/\s+/', ' ', $str);
-		if (!Path::$conf['space']) {
+		if (empty(Path::$conf['space'])) {
 			$str = preg_replace('/\s/', '-', $str);
 		}
 		if (mb_strlen($str) > 50) $str = md5($str);//У файловых систем есть ограничение на длину имени файла
