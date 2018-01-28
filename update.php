4e117c723216a85cb45a756b1e715ead708696ad
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-01-28 19:15:32 +0400

    fix auto mk dir

--- update.php
+++ update.php
@@ -2,5 +2,8 @@
 use infrajs\path\Path;
 
 Path::mkdir(Path::$conf['cache']);
-Path::mkdir(Path::$conf['data']);
-Path::mkdir(Path::$conf['auto']);
+//Path::mkdir(Path::$conf['data']); //Нужно создавать вручную, если надо.
+if (Path::theme(Path::$conf['data'])) {
+	//Если вручную не создана data значит auto тоже не будет использоваться
+	Path::mkdir(Path::$conf['auto']); //Папка для писем с сайта, и разных генерируемых данных, которые нельзя удалять как кэш.
+}
