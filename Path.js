7870c25012661a994eab15d1d03e2a48268e49c7
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2019-10-25 14:44:42 +0400

    fix

--- Path.js
+++ Path.js
@@ -11,7 +11,7 @@ window.Path = {
 
 		var conf = Config.get('path');
 		str = str.replace(/[\+]/g,'p');
-		str = str.replace(/[\'\`"\.×,№%\*<>\‐\-\'"\|\;\:\/\\\\#\!\?\$&\s]/g,' ');
+		str = str.replace(/[\'\`"\.×,№%\*<>\‐\-\–\'"\|\;\:\/\\\\#\!\?\$&\s]/g,' ');
 		if (!conf.parenthesis) str = str.replace(/[\(\)]/g,' ');
 
 		str = str.replace(/^\s+/g,'');
