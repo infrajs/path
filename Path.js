efd4a62a416469ab8ee06e940cc783f27b62f407
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-12 20:32:02 +0400

    fix

--- Path.js
+++ Path.js
@@ -1,43 +1,42 @@
-if(!window.infra) window.infra = {};
-window.Path = {
+let Path = {
 	theme: function (path) {
 		if (/^http/.test(path)) return path;
 		if (/^\//.test(path)) return path;
-		return '/'+path;
+		return '/' + path;
 	},
-	translit: function ( str ) {
-	    var ru = {
-	        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 
-	        'е': 'e', 'ё': 'e', 'ж': 'j', 'з': 'z', 'и': 'i', 'й': 'i', 
-	        'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 
-	        'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 
-	        'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh', 
-	        'щ': 'shch', 'ы': 'y', 'э': 'e', 'ю': 'yu', 'я': 'ya'
-	    }, n_str = [];
-	    
-	    str = str.replace(/[ъь]+/g, '');
-	    
-	    for ( var i = 0; i < str.length; ++i ) {
-	       n_str.push(ru[ str[i] ] || str[i]);
-	    }
-	    
-	    return n_str.join('');
+	translit: function (str) {
+		var ru = {
+			'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+			'е': 'e', 'ё': 'e', 'ж': 'j', 'з': 'z', 'и': 'i', 'й': 'i',
+			'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o',
+			'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+			'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+			'щ': 'shch', 'ы': 'y', 'э': 'e', 'ю': 'yu', 'я': 'ya'
+		}, n_str = [];
+
+		str = str.replace(/[ъь]+/g, '');
+
+		for (var i = 0; i < str.length; ++i) {
+			n_str.push(ru[str[i]] || str[i]);
+		}
+
+		return n_str.join('');
 	},
 	encode: function (str, space) //forFS
 	{
 		//Описание в php файле
 
 		var conf = Config.get('path');
-		str = str.replace(/[\+]/g,'p');
-		str = str.replace(/[\'\`"\.×,№%\*<>\‐\-\–\'"\|\;\:\/\\\\#\!\?\$&\s]/g,' ');
-		if (!conf.parenthesis) str = str.replace(/[\(\)]/g,' ');
+		str = str.replace(/[\+]/g, 'p');
+		str = str.replace(/[\'\`"\.×,№%\*<>\‐\-\–\'"\|\;\:\/\\\\#\!\?\$&\s]/g, ' ');
+		if (!conf.parenthesis) str = str.replace(/[\(\)]/g, ' ');
 
 		if (conf.encodelower || conf.translit) str = str.toLowerCase();
 		if (conf.translit) str = Path.translit(str);
 
-		str = str.replace(/^\s+/g,'');
-		str = str.replace(/\s+$/g,'');
-		str = str.replace(/\s+/g,' ');
+		str = str.replace(/^\s+/g, '');
+		str = str.replace(/\s+$/g, '');
+		str = str.replace(/\s+/g, ' ');
 
 		var conf = Config.get('path');
 		//if (!conf.space) 
@@ -47,3 +46,5 @@ window.Path = {
 		return str;
 	}
 }
+window.Path = Path
+export { Path }
--- URN.js
+++ URN.js
@@ -4,37 +4,34 @@
  * request2 не содержит знак = до первого символа /
  **/
 let URN = {
-	explode: function (separator, str, limit)
-	{
+	explode: function (separator, str, limit) {
 		var arr = str.split(separator);
-		if (limit && arr.length>1) arr.push( arr.splice(limit-1).join(separator) );
+		if (limit && arr.length > 1) arr.push(arr.splice(limit - 1).join(separator));
 		return arr;
 	},
-	str_repeat: function (str, len)
-	{	// Repeat a string
+	str_repeat: function (str, len) {	// Repeat a string
 		var res = '';
-		for (var i = 0; i < len; i++){
+		for (var i = 0; i < len; i++) {
 			res += str;
 		}
 		return res;
 	},
-	parse: function(query)
-	{
-		return Once.exec('infrajs::URN::parse '+query, function () {
-			if (typeof(query)=='undefined') query = URN.getQuery();
-			
-			var res={query: query};
+	parse: function (query) {
+		return Once.exec('infrajs::URN::parse ' + query, function () {
+			if (typeof (query) == 'undefined') query = URN.getQuery();
+
+			var res = { query: query };
 			var p = URN.explode('?', query, 2);
-			res['request']=p[0];
-			if (p.length==2) {
+			res['request'] = p[0];
+			if (p.length == 2) {
 				res['param'] = p[1];
 			} else {
 				res['param'] = '';
 			}
 			//?(catalog&m=asdf) - /(catalog?m=asdf)
-			var amp1=URN.explode('?', res['param'], 2);
-			var amp2=URN.explode('&', res['param'], 2);
-			amp=amp1[0].length<amp2[0].length?amp1:amp2;//Кто раньше встретится ? или &
+			var amp1 = URN.explode('?', res['param'], 2);
+			var amp2 = URN.explode('&', res['param'], 2);
+			amp = amp1[0].length < amp2[0].length ? amp1 : amp2;//Кто раньше встретится ? или &
 
 			var eq = URN.explode('=', amp[0], 2);
 
@@ -43,44 +40,40 @@ let URN = {
 				//В первой крошке нельзя использовать символ "=" для совместимости с левыми параметрами для главной страницы, которая всё равно покажется
 				res['request2'] = '';
 				res['param2'] = res['param'];
-				
+
 			} else {
 				res['request2'] = amp[0];
-				res['param2'] = amp.length==2 ? amp[1] : '';
-				
+				res['param2'] = amp.length == 2 ? amp[1] : '';
+
 			}
 			//Вычитаем из uri папки которые также находятся в видимости вебсервера
 			//Чтобы получить на какую глубину надо отойти от текущего uri чтобы попасть в корень вебсервера
-			var req=URN.explode('/', res['request']);
-			var deep=req.length-1;
+			var req = URN.explode('/', res['request']);
+			var deep = req.length - 1;
 
-			res['root']=URN.str_repeat('../', deep);
+			res['root'] = URN.str_repeat('../', deep);
 			return res;
 		});
 	},
-	getRoot: function ()
-	{
+	getRoot: function () {
 		var query = URN.getQuery();
 		var res = URN.parse(query);
 		return res['root'];
 	},
-	getAbsRoot: function ()
-	{
+	getAbsRoot: function () {
 		var a = URN.analize();
 		return a['root'];
 	},
-	getQuery: function ()
-	{
-		var a =URN.analize();
+	getQuery: function () {
+		var a = URN.analize();
 		return a['query'];
 	},
-	analize: function ()
-	{
+	analize: function () {
 		var query = decodeURI(window.location.pathname.substr(1));
-		if (location.search) query+=decodeURI(location.search);
+		if (location.search) query += decodeURI(location.search);
 		return { 'root': '/', 'query': query };
 	}
 }
-window.URN=URN
+window.URN = URN
 
-export {URN}
+export { URN }
